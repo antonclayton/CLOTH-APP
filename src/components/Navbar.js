ce8cccbc78a4487d66c99559ee532bef8d827acc
antonclayton <anton.clayton@gmail.com>
2024-02-02 15:49:41 -0800

    minor change

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -5,15 +5,18 @@ import { Stack } from '@mui/material';
 const Navbar = () => {
   return (
     <Stack className='navbar'
-    direction='row'
+    direction='row'                 //styles
     justifyContent='flex-end'
     alignItems='baseline'
     backgroundColor="#000000">
-        <div className='leftSide'>
 
-        </div>
+        <Stack className='leftSide'> 
+            
+        </Stack>
 
-        <Stack className='rightSide' justifyContent='baseline' direction='row'>
+        <Stack className='rightSide' 
+        justifyContent='baseline' 
+        direction='row'> 
             <Link to='/' className='link' style={{padding:'30px 100px', color: 'white', fontSize: '24px', textDecoration: 'none'}}>Search</Link>
             <Link to='/about' className='link'style={{padding: '30px 100px', color: 'white', fontSize: '24px', textDecoration: 'none'}}>About</Link>
             <Link to='/profile' className='link' style={{padding:'30px 100px', color: 'white', fontSize: '24px', textDecoration: 'none'}}>Profile</Link>
