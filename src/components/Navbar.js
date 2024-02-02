6f10351586277d4d115adbc050072487dc83f4d3
antonclayton <anton.clayton@gmail.com>
2024-02-01 21:15:07 -0800

    css changes

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -7,18 +7,18 @@ const Navbar = () => {
     <Stack className='navbar'
     direction='row'
     justifyContent='space-around'
-    sx={{gap: '100px', height: "100px", paddingTop: '20px'}}
-    backgroundColor="#DDDDDD"
+    sx={{gap: '100px', height: "100px"}}
+    backgroundColor="#000000"
     display= 'flex' flexDirection='right'>
         <div className='leftSide'>
 
         </div>
 
-        <div className='rightSide'>
-            <Link to='/' style={{padding:'50px'}}>Search</Link>
-            <Link to='/about' style={{padding:'50px'}}>About</Link>
-            <Link to='/profile' style={{padding:'50px'}}>Profile</Link>
-        </div>
+        <Stack className='rightSide' justifyContent='baseline' direction='row'>
+            <Link to='/' style={{padding:'35px 100px', color: 'white'}}>Search</Link>
+            <Link to='/about' style={{padding:'35px 100px', color: 'white'}}>About</Link>
+            <Link to='/profile' style={{padding:'35px 0px 35px 100px', color: 'white'}}>Profile</Link>
+        </Stack>
     </Stack>
   )
 }
