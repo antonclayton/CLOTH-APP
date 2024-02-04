134efcf3d6e37153afc466347e8ab903442a988e
antonclayton <anton.clayton@gmail.com>
2024-02-03 19:09:38 -0800

    Added search bar in home and a button below it (don't do anything yet)

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,8 +1,20 @@
 import React from 'react'
+import { TextField, Box, Stack, Typography, Button } from '@mui/material'
 
 const Home = () => {
   return (
-    <div>Home</div>
+    <Box
+    display='flex'
+    justifyContent='center'
+    margin='200px 0px'>
+      <Stack width='50%'>
+        <Typography variant='h4' padding='20px 20px 20px 40px'>
+          Search for clothes:
+        </Typography>
+        <TextField style={{padding: '0px 0px 10px 0px'}}label='' variant='outlined'/>
+        <Button style={{width: '25%'}} variant='contained' color='secondary'>Search</Button>
+      </Stack>
+    </Box>
   )
 }
 
