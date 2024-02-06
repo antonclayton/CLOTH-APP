4dd1c2b3158f4943f4fe0786bb02b2317209c5eb
TAAG1 <aetiusgular@gmail.com>
2024-02-06 00:07:49 -0800

    Footer added to home page

--- src/pages/Footer.js
+++ src/pages/Footer.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import './Footer.css';
-import { Button } from './Button';
+import { Button } from '../components/Button';
 import { Link } from 'react-router-dom';
 
 function Footer() {
@@ -8,22 +8,8 @@ function Footer() {
     <div className='footer-container'>
       <section className='footer-subscription'>
         <p className='footer-subscription-heading'>
-          Join the Adventure newsletter to receive our best vacation deals
+          Help us help you find some new clothes, sign up today!
         </p>
-        <p className='footer-subscription-text'>
-          You can unsubscribe at any time.
-        </p>
-        <div className='input-areas'>
-          <form>
-            <input
-              className='footer-input'
-              name='email'
-              type='email'
-              placeholder='Your Email'
-            />
-            <Button buttonStyle='btn--outline'>Subscribe</Button>
-          </form>
-        </div>
       </section>
       <div class='footer-links'>
         <div className='footer-link-wrapper'>
