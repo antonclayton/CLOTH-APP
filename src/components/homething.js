f46e224c59f475da8fce8c2d23c1229b9cf95385
antonclayton <anton.clayton@gmail.com>
2024-02-06 17:21:06 -0800

    text change

--- src/components/homething.js
+++ src/components/homething.js
@@ -5,7 +5,7 @@ import './homething.css';
 function HomeThing() {
     return (
         <div className='home-container'>
-            <h1>Welcome to to the site</h1>
+            <h1>Welcome to the site</h1>
         </div>
     )
 }
