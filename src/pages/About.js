b75d4cfc208ee67f227242937f82ef889d8a4631
antonclayton <anton.clayton@gmail.com>
2024-02-06 17:51:47 -0800

    added about page changes

--- src/pages/About.js
+++ src/pages/About.js
@@ -5,11 +5,19 @@ import '../styles/About.css'
 const About = () => {
   return (
     <div className='about-container'>
-      <Stack>
-        <Typography variant='h2'>About CLOTH</Typography>
-      </Stack>
+      <h1 className='about-header'>About CLOTH</h1>
+      <p className='about-paragraph'>CLOTH is a search engine for secondhand / vintage clothes that pulls from websites such as Grailed, Mercari, Poshmark, etc</p>
     </div>
   )
 }
+
+{/* <Stack
+      direction='column'
+      justifyContent='center'
+      alignItems='center'
+      padding='0 0 100px 0'>
+        <Typography variant='h2' padding='0 0 40px 0'>About CLOTH</Typography>
+        <Typography variant='h5'>CLOTH is a search engine for secondhand / vintage clothes that pulls from websites such as Grailed, Mercari, Poshmark, etc</Typography>
+      </Stack> */}
 
 export default About
