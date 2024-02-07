e2d9936948eef046f4a1f5d12391a61f85746762
TAAG1 <aetiusgular@gmail.com>
2024-02-06 18:04:16 -0800

    some init msg

--- src/components/homething.js
+++ src/components/homething.js
@@ -1,11 +1,25 @@
 import React from 'react'
 import '../App.css';
 import '../styles/homething.css';
+import { Button } from '../components/Button';
 
 function HomeThing() {
     return (
         <div className='home-container'>
-            <h1>Welcome to the site</h1>
+            <section className='footer-subscription'>
+            
+        <div className='input-areas'>
+          <form>
+            <input
+              className='user-input'
+              name='email'
+              type='input'
+              placeholder='Search'
+            />
+            <Button buttonStyle='btn--outline'>Search</Button>
+          </form>
+        </div>
+      </section>
         </div>
     )
 }
